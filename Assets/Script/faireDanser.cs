13427b35fb3ee6d587ce0cc509247d1b2df1b0bf
nvonaesch <nicolas.vonaesch1@gmail.com>
2024-11-06 19:25:20 +0100

    Animation fonctionne happy happy happy

---
+++ Assets/Script/faireDanser.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class faireDanser : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start(){
+        animator = gameObject.GetComponent<Animator>();
+        animator.Play("Danse");
+    }
+
+}
